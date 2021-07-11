b532d7ba34a9c1d81a2332d2c3aa2742cbd07700
vk46 <vk.hack46@gmail.com>
2021-07-12 00:01:32 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { interval } from 'rxjs/internal/observable/interval';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular-CountDown-Custom';
+
+  private subscription!: Subscription;
+
+  public dateNow = new Date();
+  public dDay = new Date('Jan 01 2022 00:00:00');
+  milliSecondsInASecond = 1000;
+  hoursInADay = 24;
+  minutesInAnHour = 60;
+  SecondsInAMinute = 60;
+
+  public timeDifference!: number;
+  public secondsToDday!: number;
+  public minutesToDday!: number;
+  public hoursToDday!: number;
+  public daysToDday!: number;
+
+
+  private getTimeDifference() {
+    this.timeDifference = this.dDay.getTime() - new Date().getTime();
+    this.allocateTimeUnits(this.timeDifference);
+  }
+
+  private allocateTimeUnits(timeDifference: any) {
+    this.secondsToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond) % this.SecondsInAMinute);
+    this.minutesToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour) % this.SecondsInAMinute);
+    this.hoursToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute) % this.hoursInADay);
+    this.daysToDday = Math.floor((timeDifference) / (this.milliSecondsInASecond * this.minutesInAnHour * this.SecondsInAMinute * this.hoursInADay));
+  }
+
+  ngOnInit() {
+    this.subscription = interval(1000)
+      .subscribe(x => { this.getTimeDifference(); });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+}
